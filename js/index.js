cd7108b3351c1dd544285948d3a896bd9f25dc8a
David Nesbitt <neztypezero@me.com>
2024-02-16 16:41:38 -0800

    added semicolons

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
-const { createHandler } = require("graphql-http/lib/use/express")
-const { buildSchema } = require("graphql")
-const { ruruHTML } = require("ruru/server")
+const { createHandler } = require("graphql-http/lib/use/express");
+const { buildSchema } = require("graphql");
+const { ruruHTML } = require("ruru/server");
 
 const app = express(); 
 
