cb82015c49beb0c025440f44394ebd306734f420
David Nesbitt <neztypezero@me.com>
2024-02-16 16:34:06 -0800

    added graphql

--- js/index.js
+++ js/index.js
@@ -1,13 +1,42 @@
-const express = require("express"); 
+const express = require("express");
+const { createHandler } = require("graphql-http/lib/use/express");
+const { buildSchema } = require("graphql");
+const { ruruHTML } = require("ruru/server")
 
-const app = express(); 
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+
+// The rootValue provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return "Hello world!"
+  },
+}
+
+const app = express();
+
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
 
 app.get("/", (req, res) => { 
 	res.send("Express on Vercel"); 
 }); 
 
 const PORT = process.env.PORT || 9874; app.listen(PORT, () => { 
-	console.log(`Server is running on port ${PORT}`); 
+  res.type("html")
+  res.end(ruruHTML({ endpoint: "/graphql" }))
 });
+
 
 module.exports = app;
